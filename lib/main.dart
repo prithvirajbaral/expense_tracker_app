3e5e4e00df566614a414c41e79f0b8ed9d6f5169
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-29 15:34:09 +0530

    create Expenses custom widget/ class and add to its home and import

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
-// import 'package:expense_tracker_app/expenses.dart';
+import 'package:expense_tracker_app/expenses.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      // home: ...,
+      home: Expenses(),
     ),
   );
 }
