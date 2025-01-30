7245229c500acee10eea73a7ac8f022db54988ae
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-30 11:11:38 +0530

    has dummy data of expence and show ExpensesList()

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,38 @@
+import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        category: Category.work,
+        title: "Flutter Course",
+        amount: 100,
+        date: DateTime.now()),
+    Expense(
+        category: Category.food,
+        title: "pizza",
+        amount: 10,
+        date: DateTime.now()),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text("The Chart..."),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
