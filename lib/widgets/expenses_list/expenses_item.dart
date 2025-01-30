1d9383d781da6eee23a200b79347ed14f20b4c32
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-30 12:21:46 +0530

    add categoryIcon and expense.formattedData

--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -24,11 +24,11 @@ class ExpensesItem extends StatelessWidget {
                   const Spacer(),
                   Row(
                     children: [
-                      const Icon(Icons.alarm),
+                      Icon(categoryIcon[expense.category]),
                       const SizedBox(
                         width: 8,
                       ),
-                      Text(expense.date.toString())
+                      Text(expense.formattedData)
                     ],
                   )
                 ],
