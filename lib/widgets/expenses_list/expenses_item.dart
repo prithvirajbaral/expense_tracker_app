b8064eb2251a40f4490c59795bd8670b44acb8c8
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-30 11:21:56 +0530

    desing for indivisual list

---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,40 @@
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem({super.key, required this.expense});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(
+            children: [
+              Text(expense.title),
+              const SizedBox(
+                height: 4,
+              ),
+              Row(
+                children: [
+                  Text(
+                      '\$${expense.amount.toStringAsFixed(2)}'), // 12.3433 -> 12.34
+                  const Spacer(),
+                  Row(
+                    children: [
+                      const Icon(Icons.alarm),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(expense.date.toString())
+                    ],
+                  )
+                ],
+              )
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,14 @@
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/expenses_list/expenses_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+  final List<Expense> expenses;
+  @override
+  Widget build(context) {
+    return ListView.builder(
+        itemCount: expenses.length,
+        itemBuilder: (ctx, index) => ExpensesItem(expense: expenses[index]));
+  }
+}
