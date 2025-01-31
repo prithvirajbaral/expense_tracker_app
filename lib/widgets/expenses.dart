c6f2b62a2b5d21f94aa1fe94718192c1df309b9f
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-31 14:53:38 +0530

    make isScrollControlled: true, to pick the form to the top of screen.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,6 +28,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverLay() {
     showModalBottomSheet(
+        isScrollControlled: true,
         context: context,
         builder: (ctx) => NewExpense(onAddExpense: _addExpenses));
   }
