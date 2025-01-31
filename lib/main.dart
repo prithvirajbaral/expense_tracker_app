36582d803bc793b582654498234952e0703a3068
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-31 17:04:26 +0530

    "Modified the app theme to set a custom scaffold background color using copyWith(), ensuring that existing theme properties remain unchanged."

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Expenses(),
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+      scaffoldBackgroundColor: const Color.fromARGB(255, 187, 227, 239), // Only changes background color
     ),
-  );
+    home: const Expenses(),
+  ));
 }
