b157974efcb0762a117ddaac23739dab5467865b
prithvirajbaral <prithvirajbaral@gmail.com>
2025-01-31 17:54:51 +0530

    Refactored theme configuration by introducing a ColorScheme for consistency. Added custom styling for AppBar, Card, ElevatedButton, and TextTheme. This improves UI uniformity and enhances user experience.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 171, 230, 236));
+
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData().copyWith(
-      scaffoldBackgroundColor: const Color.fromARGB(255, 187, 227, 239), // Only changes background color
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: kColorScheme.onSecondaryContainer,
+                    fontSize: 24),
+              )),
+      home: const Expenses(),
     ),
-    home: const Expenses(),
-  ));
+  );
 }
