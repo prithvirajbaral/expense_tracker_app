b09aee11409f0026c2a091fb58eb1c27110b163c
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-03 14:44:22 +0530

    color changed in kColorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 171, 230, 236),
+  seedColor: const Color.fromARGB(255, 115, 216, 227),
 );
 
 var kDarkColorScheme = ColorScheme.fromSeed(
